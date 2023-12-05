f0d1ab45bbf68950af756c06f866aa2c953bcabe
abhishek-rabidas <avisec07@gmail.com>
2023-12-05 13:11:53 +0530

    project init

---
+++ src/main/java/busticketbooking/BusticketbookingApplication.java
@@ -0,0 +1,13 @@
+package busticketbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusticketbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusticketbookingApplication.class, args);
+	}
+
+}
